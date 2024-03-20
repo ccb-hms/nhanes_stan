ce10831cd92e09ef73d3c66820ffac9b96656d7e
Andrew Ghazi <andghazi@gmail.com>
2024-03-20 10:38:32 -0400

    one echo

--- beautify_rstudio.sh
+++ beautify_rstudio.sh
@@ -13,10 +13,10 @@ echo "cli::cli_alert_info('Learn about Stan modeling here: {.url https://mc-stan
 echo "suppressMessages(cmdstanr::set_cmdstan_path('/.cmdstan/cmdstan-2.34.1'))" >> /usr/local/lib/R/etc/Rprofile.site
 
 # Open the demo R script. Might want to do this in a way that doesn't open every time the container starts.
-echo "setHook('rstudio.sessionInit', function(newSession) {" >> /usr/local/lib/R/etc/Rprofile.site
-echo "  if (newSession) {" >> /usr/local/lib/R/etc/Rprofile.site
-echo "    rstudioapi::documentOpen('/install_stan/demo.R')" >> /usr/local/lib/R/etc/Rprofile.site
-echo "  }" >> /usr/local/lib/R/etc/Rprofile.site
-echo "}, action = 'append')" >> /usr/local/lib/R/etc/Rprofile.site
+echo "setHook('rstudio.sessionInit', function(newSession) {
+  if (newSession) {
+    rstudioapi::documentOpen('/install_stan/demo.R')
+  }
+}, action = 'append')" >> /usr/local/lib/R/etc/Rprofile.site
 
 
